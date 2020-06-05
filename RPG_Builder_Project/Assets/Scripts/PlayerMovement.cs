42a176a56eee445675d45b90f85bdb8730a15205
Jorge Antonio Velasquez <javelasquez2001@gmail.com>
2020-06-05 12:38:25 -0400

    added a basic player controller

---
+++ RPG_Builder_Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 6f;
+    public float gravity = 9.8f;
+    private float height = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        if (controller.isGrounded)
+        {
+            height = 0f;
+        } else
+        {
+            height -= gravity * Time.deltaTime;
+        }
+        
+        Vector3 direction = new Vector3(horizontal, height, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+            controller.Move(direction * speed * Time.deltaTime);
+        }
+    }
+}
